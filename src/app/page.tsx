8fbb64ad48a8c09f5a723cc3fe8cd4043b175dda
Marcus Rådell <marcus@radell.net>
2024-01-04 08:46:56 +0100

    refactor button to a component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+const Button = () => {
+  return <button>Ok</button>;
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button>Ok</button>
+      <Button />
     </main>
   );
 }
